b85bc5accf153328be3dd4da84ff5cfdb2390a90
EstefaniaJuarez <40004754+EstefaniaJuarez@users.noreply.github.com>
2018-07-01 21:11:38 -0500

    Update NeedsChecks.py

--- NeedsChecks.py
+++ NeedsChecks.py
@@ -24,9 +24,15 @@ def scalarVecMulti(scalar,vector):
   '''
   This function takes in a scalar and vector as its inputs. The scalar is multiplied to each element of the vector. The result is then placed into the new vector, x.
   '''
+  inputStatus=True
   x = []
   #This for loop takes the multiplication of each element of the vector and the scalar and appends the product to new vector x.
   for i in range(len(vector)):
+    if ((type(vector[i]) != int) and (type(vector[i]) != float) and (type(vector[i]) != complex)):
+      inputStatus = False
+      print("Invalid Input")
+  # If the input is valid the function continues to compute the product of the scalar and vector
+  if inputStatus == True:
     x.append(vector[i]*scalar)
   return x
 
@@ -37,11 +43,17 @@ def dot(vector01,vector02):
   '''
   n = len(vector01)
   p = len(vector02)
+  inputStatus=True
   #The following line is checking that the length of both vectors is equal to ensure that multiplication is possible
   if n==p:
     x=0
     #This for loop is taking each  corresponding element, multiplying them, and adding them together for the final integer.
-    for i in range(len(vector01)):
+    for i in range(len(vector01)):  
+    if ((type(vector01[i] and vector02[i]) != int) and (type(vector01[i] and vector02[i]) != float) and (type(vector01[i and vector02[i]) != complex)):
+      inputStatus = False
+      print("Invalid Input")
+  # If the input is valid the function continues to compute the dot product
+  if inputStatus == True:
       x += vector01[i]*vector02[i]
     return x 
   else:
